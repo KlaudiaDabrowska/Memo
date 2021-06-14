79034df29c7e3cb4f6e318ddabb772bf85ad4382
Klaudia Dabrowska <kosaa96@gmail.com>
2021-06-14 12:47:22 +0200

    Added function to match and unmatch cards

--- main.js
+++ main.js
@@ -1,9 +1,40 @@
 const images= document.querySelectorAll("img");
 const cards=document.querySelectorAll(".card");
 
+let openedCards=[];
+
 function displayCard(){
-    console.log(this.children)
     this.children[0].classList.toggle("visible");
+   openCard(this)
+}
+
+function openCard(card){
+    openedCards.push(card);
+    if(openedCards.length===2){
+        if(openedCards[0].className===openedCards[1].className){
+            matched();
+    }
+        else{
+            unmatched();
+        }
+    }
+}
+    
+
+console.log(openedCards)
+const matched=()=>{
+        openedCards[0].children[0].classList.add("match");
+        openedCards[1].children[0].classList.add("match");
+        openedCards=[]
+}
+
+
+const unmatched=()=>{
+    setTimeout(function(){
+        openedCards[0].children[0].classList.toggle("visible");
+        openedCards[1].children[0].classList.toggle("visible");
+        openedCards=[]
+    }, 500)
 }
 
 
